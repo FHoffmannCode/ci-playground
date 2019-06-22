15dd5414b1e2568c3c92aa7007dad82234a97cad
Filip <fhoffmanncode@gmail.com>
2019-06-22 12:53:58 +0200

    add CI

--- django_playground/django_playground/settings/docker.py
+++ django_playground/django_playground/settings/docker.py
@@ -76,14 +76,6 @@ WSGI_APPLICATION = 'django_playground.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'django_playground',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
--- django_playground/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-#pip install -r /utils/requirements.txt
-
-python manage.py makemigrations
-python manage.py migrate
-
-python manage.py runserver
